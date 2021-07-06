071feee6bcf66fc457894a97468c73bf8b73e555
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2021-07-06 16:16:49 -0300

    Add basic wiki page models

--- server/applications/wiki/models.py
+++ server/applications/wiki/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class WikiPage(models.Model):
+    """ The wiki page model
+
+    Wiki is made of this pages, each one is a core to all metadata of them self
+    """
+    title = models.CharField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class WikiPageModification(models.Model):
+    """ Wiki page modification
+
+    Each modification store the previus state of the page at the modification time
+    """
+    update_date = models.DateTimeField(auto_now_add=True)
+    old_content = models.TextField()
+
+    def __str__(self):
+        return self.update_date
+
+class WikiTag(self):
+    """ A tag used to find content
+
+    Each page uses tags as a guide to search more effective instead of keywords
+    """
+    tag_name = models.CharField()
