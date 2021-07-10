a69b34811fa6e3290c48be1dfaf23e7d32ef0653
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2021-07-10 17:57:45 -0300

    Add wiki page detail view and template

--- server/applications/wiki/views.py
+++ server/applications/wiki/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import WikiPage
 
 # Create your views here.
+class WikiPageDetailView(DetailView):
+    """ Lite detail view of a wiki page
+    """
+
+    model = WikiPage
+    template_name = 'wiki/wikipage_detail.html'
